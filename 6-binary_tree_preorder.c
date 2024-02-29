39d5dcfca5db40fd3ce53f8016d34ef1d6efe2f3
zhabwe254 <zhabwe@gmail.com>
2024-02-29 20:00:25 +0300

    a

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Function to perform pre-order traversal on a binary tree 
- * @tree: the tree to traverse 
+ * binary_tree_preorder - perform pre-order traversal on a binary tree
+ * @tree: the tree to traverse
  * @func: Call the function for the current node's value
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
