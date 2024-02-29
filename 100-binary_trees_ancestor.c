6d64d94002798a1a4094ed97fb55f6f5303ea5d0
zhabwe254 <zhabwe@gmail.com>
2024-02-29 17:33:41 +0300

    a

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -4,53 +4,44 @@
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
  * @first: Pointer to the first node
  * @second: Pointer to the second node
- * Return: Pointer to the lowest common ancestor, NULL if not found
+ *
+ * Return: Pointer to the lowest common ancestor node
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	if (!first || !second)
-		return (NULL);
-
-	const binary_tree_t *temp1, *temp2;
-	size_t depth1 = 0, depth2 = 0;
-
-	temp1 = first;
-	temp2 = second;
-
-	while (temp1->parent)
-	{
-		temp1 = temp1->parent;
-		depth1++;
-	}
-
-	while (temp2->parent)
-	{
-		temp2 = temp2->parent;
-		depth2++;
-	}
-
-	temp1 = first;
-	temp2 = second;
-
-	while (depth1 > depth2)
-	{
-		temp1 = temp1->parent;
-		depth1--;
-	}
-
-	while (depth2 > depth1)
-	{
-		temp2 = temp2->parent;
-		depth2--;
-	}
-
-	while (temp1 && temp2)
-	{
-		if (temp1 == temp2)
-			return ((binary_tree_t *)temp1);
-		temp1 = temp1->parent;
-		temp2 = temp2->parent;
-	}
-
 	return (NULL);
+
+	if (first == second)
+	return ((binary_tree_t *)first);
+
+	if (binary_tree_is_descendant(first, second))
+		return ((binary_tree_t *)second);
+
+	if (binary_tree_is_descendant(second, first))
+	return ((binary_tree_t *)first);
+
+	return (binary_trees_ancestor(first->parent, second));
 }
+
+/**
+ * binary_tree_is_descendant - Checks if a node is a descendant of another
+ * @root: Pointer to the potential ancestor node
+ * @node: Pointer to the potential descendant node
+ *
+ * Return: 1 if node is descendant of root, 0 otherwise
+ */
+int binary_tree_is_descendant(const binary_tree_t *root, const binary_tree_t *node)
+{
+	if (!root || !node)
+		return (0);
+
+	while (node != NULL)
+{
+	if (node == root)
+		return (1);
+	node = node->parent;
+	}
+	return (0);
+}
+
