e638b3cb3957221c4ecfd8b10bcd17eea55dc465
zhabwe254 <zhabwe@gmail.com>
2024-02-29 19:05:46 +0300

    A

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,28 +1,49 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left - Performs a left-rotation on a binary tree
- * @tree: Pointer to the root node of the tree to rotate
- * Return: Pointer to the new root node of the tree once rotated
+ * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: pointer to the root of the BST
+ * @value: value to insert
+ *
+ * Return: pointer to the inserted node, or NULL on failure
  */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (!tree || !tree->right)
+	if (!tree)
 		return (NULL);
 
-	binary_tree_t *new_root = tree->right;
-	tree->right = new_root->left;
-	if (new_root->left)
-		new_root->left->parent = tree;
-	new_root->parent = tree->parent;
-	if (!tree->parent)
-		new_root->parent = NULL;
-	else if (tree->parent->left == tree)
-		tree->parent->left = new_root;
-	else
-		tree->parent->right = new_root;
-	new_root->left = tree;
-	tree->parent = new_root;
+	if (!*tree)
+	{
+		*tree = binary_tree_node(NULL, value);
+		if (!*tree)
+			return (NULL);
+		return (*tree);
+	}
 
-	return (new_root);
+	if (value < (*tree)->n)
+	{
+		if (!(*tree)->left)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			if (!(*tree)->left)
+				return (NULL);
+			return ((*tree)->left);
+		}
+		else
+			return (bst_insert(&((*tree)->left), value));
+	}
+	else if (value > (*tree)->n)
+	{
+		if (!(*tree)->right)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			if (!(*tree)->right)
+				return (NULL);
+			return ((*tree)->right);
+		}
+		else
+			return (bst_insert(&((*tree)->right), value));
+	}
+
+	return (NULL); /* value already exists */
 }
